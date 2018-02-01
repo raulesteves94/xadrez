bf0810e8b3cc053c73236af2715dae342bbe3866
Raul Esteves <raul.m.esteves@hotmail.com>
2018-02-01 14:30:17 -0200

    Jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using tabuleiro;
 
 namespace xadrez
@@ -123,8 +124,22 @@ namespace xadrez
                 throw new TabuleiroException(("Você não pode se colocar em xeque!"));
             }
 
-            if (estaEmXeque(adversaria(jogadorAtual)))
+            Peca p = tab.peca(destino);
+
+            // jodadaespecial promocao
+            if (p is Peao)
             {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+            if (estaEmXeque(adversaria(jogadorAtual))){
                 xeque = true;
             }
             else{
@@ -139,7 +154,7 @@ namespace xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
 
             // jogadaespecial en passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2)){
